3073de0ca61285bf17ff06072ba6e5ce16e0c802
gmkt <kirilltelnoy@gmail.com>
2018-09-10 20:35:14 +0300

    Added ArraySet actually based on ArrayList

--- src/ru/ifmo/rain/telnoj/arrayset/ArraySet.java
+++ src/ru/ifmo/rain/telnoj/arrayset/ArraySet.java
@@ -3,11 +3,11 @@ package ru.ifmo.rain.telnoj.arrayset;
 import java.util.*;
 
 public class ArraySet<T extends Comparable<T>> extends AbstractSet<T> implements NavigableSet<T> {
-    private NavigableSet<T> set;
+    private List<T> list;
     private Comparator<? super T> comparator;
 
     public ArraySet() {
-        set = new TreeSet<>();
+        list = new ArrayList<>();
         comparator = null;
     }
 
@@ -16,39 +16,65 @@ public class ArraySet<T extends Comparable<T>> extends AbstractSet<T> implements
     }
 
     public ArraySet(Collection<? extends T> collection, Comparator<? super T> comparator) {
-        set = new TreeSet<>(comparator);
+        TreeSet<T> set = new TreeSet<>(comparator);
         set.addAll(collection);
+        list = new ArrayList<>(set);
         this.comparator = comparator;
     }
 
-    public ArraySet(Comparator<? super T> comparator, SortedSet<T> set) {
-        this.set = (NavigableSet<T>) set;
+    private ArraySet(List<T> list, Comparator<? super T> comparator) {
+        this.list = list;
         this.comparator = comparator;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<T>() {
+            Iterator<T> iterator = list.iterator();
+
+            public boolean hasNext() {
+                return iterator.hasNext();
+            }
+
+            public T next() {
+                return iterator.next();
+            }
+        };
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public boolean contains(Object o) {
+        int index = collectionsBinarySearch((T) o);
+        return index >= 0;
     }
 
     @Override
     public T lower(T t) {
-        return set.lower(t);
+        int index = getIndex(t);
+        if (index > 0) return list.get(index - 1);
+        return null;
     }
 
     @Override
     public T floor(T t) {
-        return set.floor(t);
+        int index = getGreaterIndex(t);
+        if (index == 0) return null;
+        return list.get(index - 1);
     }
 
     @Override
     public T ceiling(T t) {
-        return set.ceiling(t);
-    }
-
-    @Override
-    public boolean contains(Object o) {
-        return set.contains(o);
+        int index = getIndex(t);
+        if (index < list.size()) return list.get(index);
+        return null;
     }
 
     @Override
     public T higher(T t) {
-        return set.higher(t);
+        int index = getGreaterIndex(t);
+        if (index == list.size()) return null;
+        return list.get(index);
     }
 
     @Override
@@ -62,23 +88,13 @@ public class ArraySet<T extends Comparable<T>> extends AbstractSet<T> implements
     }
 
     @Override
-    public Iterator<T> iterator() {
-        return new Iterator<T>() {
-            Iterator<T> iterator = set.iterator();
-
-            public boolean hasNext() {
-                return iterator.hasNext();
-            }
-
-            public T next() {
-                return iterator.next();
-            }
-        };
-    }
-
-    @Override
-    public ArraySet<T> descendingSet() {
-        return new ArraySet<>(comparator, set.descendingSet());
+    public NavigableSet<T> descendingSet() {
+        ListIterator<T> iterator = this.list.listIterator(list.size());
+        ArrayList<T> list = new ArrayList<>(this.list.size());
+        while (iterator.hasPrevious()) {
+            list.add(iterator.previous());
+        }
+        return new ArraySet<>(list, comparator.reversed());
     }
 
     @Override
@@ -86,23 +102,21 @@ public class ArraySet<T extends Comparable<T>> extends AbstractSet<T> implements
         return descendingSet().iterator();
     }
 
-    private ArraySet<T> derivedSet(SortedSet<T> set) {
-        return new ArraySet<>(comparator, set);
-    }
-
-    @Override
-    public ArraySet<T> subSet(T fromElement, boolean fromInclusive, T toElement, boolean toInclusive) {
-        return derivedSet(set.subSet(fromElement, fromInclusive, toElement, toInclusive));
-    }
-
     @Override
     public ArraySet<T> headSet(T toElement, boolean inclusive) {
-        return derivedSet(set.headSet(toElement, inclusive));
+        if (!inclusive) return headSet(toElement);
+        return derivedHeadSet(getGreaterIndex(toElement));
     }
 
     @Override
     public ArraySet<T> tailSet(T fromElement, boolean inclusive) {
-        return derivedSet(set.tailSet(fromElement, inclusive));
+        if (inclusive) return tailSet(fromElement);
+        return derivedTailSet(getGreaterIndex(fromElement));
+    }
+
+    @Override
+    public ArraySet<T> subSet(T fromElement, boolean fromInclusive, T toElement, boolean toInclusive) {
+        return headSet(toElement, toInclusive).tailSet(fromElement, fromInclusive);
     }
 
     @Override
@@ -111,32 +125,66 @@ public class ArraySet<T extends Comparable<T>> extends AbstractSet<T> implements
     }
 
     @Override
-    public ArraySet<T> subSet(T fromElement, T toElement) {
-        return derivedSet(set.subSet(fromElement, toElement));
-    }
-
-    @Override
     public ArraySet<T> headSet(T toElement) {
-        return derivedSet(set.headSet(toElement));
+        return derivedHeadSet(getIndex(toElement));
     }
 
     @Override
     public ArraySet<T> tailSet(T fromElement) {
-        return derivedSet(set.tailSet(fromElement));
+        return derivedTailSet(getIndex(fromElement));
+    }
+
+    @Override
+    public ArraySet<T> subSet(T fromElement, T toElement) {
+        return headSet(toElement).tailSet(fromElement);
     }
 
     @Override
     public T first() {
-        return set.first();
+        return getElementIfNotEmpty(0);
     }
 
     @Override
     public T last() {
-        return set.last();
+        return getElementIfNotEmpty(list.size() - 1);
     }
 
     @Override
     public int size() {
-        return set.size();
+        return list.size();
+    }
+
+    private ArraySet<T> derivedSet(List<T> list) {
+        return new ArraySet<>(list, comparator);
+    }
+
+    private int collectionsBinarySearch(T element) {
+        return Collections.binarySearch(list, element, comparator);
+    }
+
+    private T getElementIfNotEmpty(int index) {
+        if (list.isEmpty()) throw new NoSuchElementException();
+        return list.get(index);
+    }
+
+    private int getIndex(T element) {
+        int index = collectionsBinarySearch(element);
+        if (index < 0) index = -index - 1;
+        return index;
+    }
+
+    private int getGreaterIndex(T element) {
+        int index = collectionsBinarySearch(element);
+        if (index < 0) index = -index - 1;
+        else index++;
+        return index;
+    }
+
+    private ArraySet<T> derivedHeadSet(int index) {
+        return derivedSet(list.subList(0, index));
+    }
+
+    private ArraySet<T> derivedTailSet(int index) {
+        return derivedSet(list.subList(index, list.size()));
     }
 }
